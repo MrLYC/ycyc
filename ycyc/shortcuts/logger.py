f0e23c8ca4bb66ec740ab2dab9668ceee946a86e
MrLYC <imyikong@gmail.com>
2015-04-04 21:15:56 +0800

    add logger shortcut

---
+++ ycyc/shortcuts/logger.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+def quick_config(log_file="application.log"):
+    """
+    a quick config for logging
+
+    :param log_file: the file for file handler
+    """
+    import logging.config
+    config = {
+        'version': 1,
+        'disable_existing_loggers': True,
+        'formatters': {
+            'verbose': {
+                'format': '%(asctime)s %(levelname)s %(name)s %(message)s',
+                'datefmt': '[%d/%b/%Y %H:%M:%S]',
+            },
+            'simple': {
+                'format': '%(asctime)s %(levelname)s %(name)s:%(lineno)s %(message)s',
+                'datefmt': '[%d/%b/%Y %H:%M:%S]',
+            },
+        },
+        'handlers': {
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'simple'
+            },
+            'file': {
+                'level': 'INFO',
+                'class': 'logging.FileHandler',
+                'filename': log_file,
+                'formatter': 'verbose'
+            },
+        },
+        'loggers': {
+            '': {
+                'handlers': ['console', 'file'],
+                'level': 'DEBUG',
+            },
+        },
+    }
+    logging.config.dictConfig(config)
