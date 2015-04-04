8413f92df02d179d0f901172cdc3ff7b95c02564
MrLYC <imyikong@gmail.com>
2015-04-05 01:01:45 +0800

    add ObjAsDictAdapter

---
+++ ycyc/adapters/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import collections
+
+
+class ObjAsDictAdapter(collections.Mapping):
+    def __init__(self, obj):
+        self.__object = obj
+
+    def __getitem__(self, name):
+        try:
+            return getattr(self.__object, name)
+        except AttributeError:
+            raise KeyError(name)
+
+    def __setitem__(self, name, value):
+        setattr(self.__object, name, value)
+
+    def __iter__(self):
+        return iter(dir(self.__object))
+
+    def __len__(self):
+        return len(dir(self.__object))
