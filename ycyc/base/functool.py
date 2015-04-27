972a9fcd19ce88356e0985ebb3d1a96d6afbc6f2
MrLYC <imyikong@gmail.com>
2015-04-27 11:16:02 +0800

    add bytescat and strcat

--- ycyc/base/functool.py
+++ ycyc/base/functool.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-
 import re
 
 
@@ -12,3 +11,26 @@ def template_render(template, model):
             part if len(parts) % 2 == 0 else
             reduce(lambda m, k: m[k], part.split("."), model)))
     return "".join(parts)
+
+
+def encode(s, encoding="utf-8", errors="strict"):
+    if isinstance(s, bytes):
+        return s
+
+    return s.encode(encoding, errors)
+
+
+def decode(s, encoding="utf-8", errors="strict"):
+    if isinstance(s, unicode):
+        return s
+
+    return s.decode(encoding, errors)
+
+
+def bytescat(s1, s2, encoding="utf-8"):
+    return encode(s1, encoding) + encode(s2, encoding)
+
+
+def strcat(s1, s2, encoding1="utf-8", encoding2=None):
+    encoding2 = encoding2 or encoding1
+    return decode(s1, encoding1) + decode(s2, encoding2)
