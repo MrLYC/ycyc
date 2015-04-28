5323df7b8ea1b852e5a05ece6fcb13b660dfc45e
lyc <saber000@vip.qq.com>
2015-04-29 00:28:03 +0800

    add LazyEnv

---
+++ tests/base/test_lazytool.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from ycyc.base import lazytool
+
+from unittest import TestCase
+
+
+class TestLazyEnv(TestCase):
+    def test_attr_usage(self):
+        env = lazytool.LazyEnv()
+        env.a = lambda: global_a
+
+        with self.assertRaisesRegexp(NameError, r"\bglobal_a\b"):
+            env.a
+
+        global_a = []
+        self.assertIs(env.a, global_a)
+        global_a = {}
+        self.assertIsNot(env.a, global_a)
+
+        env.a = global_a
+        self.assertIs(env.a, global_a)
+
+        global_a = ()
+        env.a = lambda: global_a
+        self.assertIs(env.a, global_a)
+
+    def test_item_usage(self):
+        import types
+
+        env = lazytool.LazyEnv()
+        env["a"] = lambda: global_a
+
+        self.assertIsInstance(env.a, types.LambdaType)
+        self.assertIsInstance(env["a"], types.LambdaType)
+
+        env.a = lambda: global_a
+        with self.assertRaisesRegexp(NameError, r"\bglobal_a\b"):
+            env.a
+
+        global_a = []
+        self.assertIs(env.a, global_a)
