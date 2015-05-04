d5de48ccdfe5fb576935ae99e382e29391f63bc5
MrLYC <imyikong@gmail.com>
2015-05-04 18:36:03 +0800

    add timeout context manager

--- ycyc/base/contextutils.py
+++ ycyc/base/contextutils.py
@@ -41,3 +41,33 @@ def subprocessor(*args, **kwg):
         if processor.poll() is None:
             processor.terminate()
             processor.wait()
+
+
+@contextmanager
+def timeout(seconds):
+    """
+    Send KeyboardInterrupt to main thread to terminate it and
+    convert it as RuntimeError if timeout.It's unreliable.
+    :param seconds: timeout seconds
+    """
+    import thread
+    import time
+
+    signal_finished = False
+
+    def poll_signal(seconds):
+        now = start = time.time()
+        while not signal_finished and now - start < seconds:
+            time.sleep(0.1)
+            now = time.time()
+        if not signal_finished:
+            thread.interrupt_main()
+        print "Done"
+
+    thread.start_new_thread(poll_signal, (seconds,))
+    try:
+        time.sleep(0)
+        yield
+        signal_finished = True
+    except KeyboardInterrupt:
+        raise RuntimeError("timeout")
