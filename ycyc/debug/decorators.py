d613faf55026c865066557de34d37cc15e925574
lyc <saber000@vip.qq.com>
2015-05-08 09:39:47 +0800

    fix call_trace and add debug_call_trace

--- ycyc/debug/decorators.py
+++ ycyc/debug/decorators.py
@@ -19,3 +19,27 @@ def pdb_break(func):
         pdb.set_trace()
         return func(*args, **kwg)
     return _
+
+
+def debug_call_trace(loger=None, name=None):
+    """
+    Trace a fun call with loger.
+    :param loger: logging.Logger
+    :param name: function name
+    """
+    def _(func):
+        fname = name or func.func_name
+
+        @functools.wraps(func)
+        def f(*args, **kwg):
+            fid = id(args) ^ id(kwg)
+            loger.info("%s[%d] call, args: %s, kwg: %s", fname, fid, args, kwg)
+            try:
+                res = func(*args, **kwg)
+                loger.info("%s[%s] return: %s", fname, fid, res)
+                return res
+            except Exception as err:
+                loger.info("%s[%d] got error: %s", fname, fid, err)
+                raise
+        return f
+    return _
