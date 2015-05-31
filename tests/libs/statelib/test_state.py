36ce41bb2a9f6decc3d016ce3e22c4a329ebbb3f
MrLYC <imyikong@gmail.com>
2015-05-31 10:44:47 +0800

    add state lib

--- tests/libs/statelib/test_state.py
+++ tests/libs/statelib/test_state.py
@@ -3,7 +3,10 @@
 
 from unittest import TestCase
 
-from ycyc.libs.statelib.state import FreeState, StateNotAllowError
+from ycyc.libs.statelib.state import (
+    FreeState, SequenceState, QSequenceState,
+    StateNotAllowError, EndOfSequenceStateError, SequenceStateFinishedError,
+)
 
 
 class TestFreeState(TestCase):
@@ -11,36 +14,87 @@ class TestFreeState(TestCase):
         state = FreeState("init")
         self.assertEqual(state.state, "init")
         self.assertEqual(state.exception, None)
-        self.assertEqual(state.from_state, None)
+        self.assertEqual(state.last_state, None)
 
         with state.processing("init", "state1", "error1"):
             pass
         self.assertEqual(state.state, "state1")
         self.assertEqual(state.exception, None)
-        self.assertEqual(state.from_state, "init")
+        self.assertEqual(state.last_state, "init")
 
         with self.assertRaises(StateNotAllowError):
             with state.processing("nothing", "", ""):
                 pass
         self.assertEqual(state.state, "state1")
         self.assertEqual(state.exception, None)
-        self.assertEqual(state.from_state, "init")
+        self.assertEqual(state.last_state, "init")
 
         with self.assertRaises(ValueError):
             with state.processing("state1", "state2", "error2"):
                 raise ValueError()
         self.assertEqual(state.state, "error2")
         self.assertIsInstance(state.exception, ValueError)
-        self.assertEqual(state.from_state, "state1")
+        self.assertEqual(state.last_state, "state1")
 
         with state.processing(("error2", "state3"), "state3", "error3"):
             pass
         self.assertEqual(state.state, "state3")
         self.assertEqual(state.exception, None)
-        self.assertEqual(state.from_state, "error2")
+        self.assertEqual(state.last_state, "error2")
 
         with state.processing(("error2", "state3"), "state4", "error4"):
             pass
         self.assertEqual(state.state, "state4")
         self.assertEqual(state.exception, None)
-        self.assertEqual(state.from_state, "state3")
+        self.assertEqual(state.last_state, "state3")
+
+class TestSequenceState(TestCase):
+    def test_usage(self):
+        class TSequenceState(SequenceState):
+            __SequenceStates__ = ("init", "state1", "state2", "state3")
+
+        state = TSequenceState()
+        self.assertEqual(state.state, "init")
+        self.assertEqual(state.index, 0)
+        self.assertEqual(state.exception, None)
+        self.assertEqual(state.last_state, None)
+        self.assertEqual(state.next_state, "state1")
+        self.assertEqual(state.is_finished, False)
+
+        with state.processing():
+            pass
+        self.assertEqual(state.state, "state1")
+        self.assertEqual(state.index, 1)
+        self.assertEqual(state.exception, None)
+        self.assertEqual(state.last_state, "init")
+        self.assertEqual(state.next_state, "state2")
+        self.assertEqual(state.is_finished, False)
+
+        with state.processing():
+            pass
+        self.assertEqual(state.state, "state2")
+        self.assertEqual(state.index, 2)
+        self.assertEqual(state.exception, None)
+        self.assertEqual(state.last_state, "state1")
+        self.assertEqual(state.next_state, "state3")
+        self.assertEqual(state.is_finished, False)
+
+        with self.assertRaises(ValueError):
+            with state.processing():
+                raise ValueError()
+        self.assertEqual(state.state, "error")
+        self.assertEqual(state.index, 2)
+        self.assertIsInstance(state.exception, ValueError)
+        self.assertEqual(state.last_state, "state2")
+        self.assertEqual(state.next_state, "state3")
+        self.assertEqual(state.is_finished, True)
+
+        with self.assertRaises(SequenceStateFinishedError):
+            with state.processing():
+                pass
+        self.assertEqual(state.state, "error")
+        self.assertEqual(state.index, 2)
+        self.assertIsInstance(state.exception, ValueError)
+        self.assertEqual(state.last_state, "state2")
+        self.assertEqual(state.next_state, "state3")
+        self.assertEqual(state.is_finished, True)
--- ycyc/libs/statelib/state.py
+++ ycyc/libs/statelib/state.py
@@ -8,34 +8,104 @@ class StateNotAllowError(Exception):
     pass
 
 
+class EndOfSequenceStateError(Exception):
+    pass
+
+
+class SequenceStateFinishedError(Exception):
+    pass
+
+
 class FreeState(object):
     def __init__(self, init_state):
-        self.from_state = None
+        self.last_state = None
         self.state = init_state
         self.exception = None
 
-    def is_in_states(self, states):
+    def check_is_in_states(self, states):
         if self.state not in states:
             raise StateNotAllowError("%s not allow" % self.state)
 
     def state_to(self, to_state):
-        self.from_state = self.state
+        self.last_state = self.state
         self.state = to_state
+
+    def on_success(self, allow_states, success_state):
+        self.check_is_in_states(allow_states)
+        self.state_to(success_state)
+        self.exception = None
+
+    def on_fail(self, allow_states, fail_state, exception):
+        self.check_is_in_states(allow_states)
+        self.state_to(fail_state)
+        self.exception = exception
 
     @contextmanager
     def processing(self, allow_states, success_state, fail_state):
         if not isinstance(allow_states, (tuple, list, set)):
             allow_states = (allow_states,)
 
-        self.is_in_states(allow_states)
+        self.check_is_in_states(allow_states)
         try:
             yield
         except Exception as err:
-            self.is_in_states(allow_states)
-            self.state_to(fail_state)
-            self.exception = err
+            self.on_fail(allow_states, fail_state, err)
             raise
         else:
-            self.is_in_states(allow_states)
-            self.state_to(success_state)
-            self.exception = None
+            self.on_success(allow_states, success_state)
+
+
+class SequenceState(FreeState):
+    __SequenceStates__ = ()
+    __FailState__ = "error"
+
+    def __init__(self, fail_state=NotImplemented):
+        if fail_state is not NotImplemented:
+            self.__FailState__ = fail_state
+        self.index = 0
+        self.base = super(SequenceState, self)
+        self.base.__init__(init_state=self.__SequenceStates__[self.index])
+
+    @property
+    def next_state(self):
+        index = self.index + 1
+        if index >= len(self.__SequenceStates__):
+            raise EndOfSequenceStateError()
+        return self.__SequenceStates__[index]
+
+    @property
+    def is_finished(self):
+        if not self.index < len(self.__SequenceStates__):
+            return True
+        if self.state is self.__FailState__:
+            return True
+        return False
+
+    def on_success(self, allow_states, success_state):
+        self.base.on_success(allow_states, success_state)
+        self.index += 1
+
+    def processing(self, allow_states=None):
+        if self.is_finished:
+            raise SequenceStateFinishedError()
+        if allow_states is None:
+            allow_states = self.state
+
+        return self.base.processing(
+            allow_states, self.next_state, self.__FailState__,
+        )
+
+
+class QSequenceState(SequenceState):
+    def __init__(self, states, fail_state=NotImplemented):
+        self.__SequenceStates__ = states
+        super(QSequenceState, self).__init__(fail_state=fail_state)
+
+
+def SequenceStateBuilder(sequence_states, fail_state=NotImplemented):
+    class NewSequenceState(SequenceState):
+        __SequenceStates__ = sequence_states
+        if fail_state is not NotImplemented:
+            __FailState__ = fail_state
+
+    return NewSequenceState
