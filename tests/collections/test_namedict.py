c08cde86d52df13a7776f0c0596d879055e432a6
lyc <saber000@vip.qq.com>
2015-06-03 14:29:52 +0800

    add namedict

---
+++ tests/collections/test_namedict.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from unittest import TestCase
+import mock
+import time
+
+from ycyc.collections.namedict import namedict, RequireFieldsMissError
+
+
+class TestNamedDict(TestCase):
+    def test_usage(self):
+        cur_time = time.time()
+        Params = namedict(
+            "InvokeParams",
+            requires=("sender", "callback"),
+            fields={
+                "value": None,
+                "time": cur_time,
+            },
+        )
+
+        self.assertEqual(Params.__name__, "InvokeParams")
+
+        with self.assertRaisesRegexp(
+            RequireFieldsMissError, ", ".join({"sender", "callback"})
+        ):
+            Params()
+
+        with self.assertRaisesRegexp(RequireFieldsMissError, "sender"):
+            Params(callback=None)
+
+        with self.assertRaisesRegexp(RequireFieldsMissError, "callback"):
+            Params(sender=None)
+
+        params = Params("test", 1)
+        self.assertIsInstance(params, dict)
+        self.assertEqual(params.sender, "test")
+        self.assertEqual(params.callback, 1)
+        self.assertEqual(params.value, None)
+        self.assertEqual(params.time, cur_time)
+
+        params = Params(sender="test", callback=2)
+        self.assertIsInstance(params, dict)
+        self.assertEqual(params.sender, "test")
+        self.assertEqual(params.callback, 2)
+        self.assertEqual(params.value, None)
+        self.assertEqual(params.time, cur_time)
+
+        params = Params("test", None, value=1)
+        self.assertIsInstance(params, dict)
+        self.assertEqual(params.sender, "test")
+        self.assertEqual(params.callback, None)
+        self.assertEqual(params.value, 1)
+        self.assertEqual(params.time, cur_time)
+
+        params = Params("test", None, value=1, time=2)
+        self.assertIsInstance(params, dict)
+        self.assertEqual(params.sender, "test")
+        self.assertEqual(params.callback, None)
+        self.assertEqual(params.value, 1)
+        self.assertEqual(params.time, 2)
+
+        Params = namedict(
+            "InvokeParams",
+            requires=("sender", "callback"),
+            fields=(
+                ("value", None),
+                ("time", cur_time),
+            ),
+        )
+
+        params = Params("test", None, 1)
+        self.assertIsInstance(params, dict)
+        self.assertEqual(params.sender, "test")
+        self.assertEqual(params.callback, None)
+        self.assertEqual(params.value, 1)
+        self.assertEqual(params.time, cur_time)
+
+        params = Params("test", None, 1, 2)
+        self.assertIsInstance(params, dict)
+        self.assertEqual(params.sender, "test")
+        self.assertEqual(params.callback, None)
+        self.assertEqual(params.value, 1)
+        self.assertEqual(params.time, 2)
+
+        params = Params("test", None, 1, value=2)
+        self.assertIsInstance(params, dict)
+        self.assertEqual(params.sender, "test")
+        self.assertEqual(params.callback, None)
+        self.assertEqual(params.value, 1)
+        self.assertEqual(params.time, cur_time)
+
+        params = Params("test", None, 1, value=2, nothing=3)
+        self.assertIsInstance(params, dict)
+        self.assertEqual(params.sender, "test")
+        self.assertEqual(params.callback, None)
+        self.assertEqual(params.value, 1)
+        self.assertEqual(params.time, cur_time)
+        with self.assertRaises(AttributeError):
+            params.nothing
+
+    def test_logging(self):
+        with mock.patch("ycyc.collections.namedict.logger"):
+            from ycyc.collections.namedict import logger
+            TestDict = namedict("TestDict", {"val": None})
+
+            tdict = TestDict(1)
+            self.assertEqual(tdict.val, 1)
+            self.assertEqual(logger.warning.call_count, 0)
+
+            tdict = TestDict(1, 2)
+            self.assertEqual(tdict.val, 1)
+            self.assertEqual(logger.warning.call_count, 1)
+            self.assertListEqual(logger.warning.call_args[0][1], [2])
+
+            tdict = TestDict(1, nothing=2)
+            self.assertEqual(tdict.val, 1)
+            self.assertEqual(logger.warning.call_count, 2)
+            self.assertSetEqual(logger.warning.call_args[0][1], {"nothing"})
---
+++ ycyc/collections/namedict.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import six
+from collections import OrderedDict
+from itertools import chain
+import logging
+
+from ycyc.base.contextutils import catch
+
+logger = logging.getLogger(__name__)
+
+
+class RequireFieldsMissError(Exception):
+    pass
+
+
+class NamedDictMeta(type):
+    def __new__(cls, name, base, attrs):
+        fields = attrs.pop("__Fields__", {})
+        attrs["__Fields__"] = OrderedDict(fields)
+
+        requires = attrs.pop("__Requires__", ())
+        attrs["__Requires__"] = tuple(requires)
+
+        return type.__new__(cls, name, base, attrs)
+
+
+class NamedDict(six.with_metaclass(NamedDictMeta, dict)):
+    def __init__(self, *args, **kwg):
+        requires = self.__Requires__
+        fields = self.__Fields__.copy()
+        field_keys = fields.viewkeys()
+        real_keys = field_keys | set(requires)
+
+        if kwg:
+            kwg_keys = kwg.viewkeys()
+            undefined_keys = kwg_keys - real_keys
+            if undefined_keys:
+                logger.warning("undefined keys: %s", undefined_keys)
+
+            fields.update({
+                k: kwg[k]
+                for k in real_keys & kwg_keys
+            })
+        if args:
+            i_args = iter(args)
+            fields.update({
+                k: v
+                for k, v in zip(chain(requires, field_keys), i_args)
+            })
+
+            remain_args = list(i_args)
+            if remain_args:
+                logger.warning("not acceptable arguments: %s", remain_args)
+
+        missed_fields = set(requires) - set(fields.keys())
+        if missed_fields:
+            logger.debug("missed_fields: %s", missed_fields)
+            raise RequireFieldsMissError(
+                "fields: [%s]" % ", ".join(missed_fields)
+            )
+
+        super(NamedDict, self).__init__(fields)
+
+    def __getattr__(self, name):
+        with catch(KeyError, reraise=AttributeError):
+            return self[name]
+
+    def __setattr__(self, name, key):
+        self[name] = key
+
+
+def namedict(name, fields, requires=()):
+    """
+    Return a subclass of dict with named fields
+
+    :param fields: option fields
+    :param requires: requires fields
+    """
+    return type(name, (NamedDict,), {
+        "__Fields__": fields,
+        "__Requires__": requires,
+    })
