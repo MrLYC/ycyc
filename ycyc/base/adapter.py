1281f69d1117b58a130e25a5e8d75181d2a99bab
MrLYC <imyikong@gmail.com>
2015-06-25 13:00:19 +0800

    add a marker

--- ycyc/base/adapter.py
+++ ycyc/base/adapter.py
@@ -29,3 +29,24 @@ class ObjAsDictAdapter(collections.Mapping):
 
     def __len__(self):
         return len(dir(self.__object))
+
+
+class Marker(object):
+    def __init__(self, name=NotImplemented):
+        self.name = "Mark[%s]" % id(self) if name is NotImplemented else name
+        self.__freezed = True
+
+    def __setattr__(self, name, val):
+        if getattr(self, "__freezed", False):
+            raise AttributeError("attribute %s is not writable" % name)
+        super(Marker, self).__setattr__(name, val)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return "%s('%s')" % (self.__class__.__name__, self.name)
+
+Marker.Undefined = Marker("undefined")
+Marker.Missed = Marker("missed")
+Marker.Disabled = Marker("disabled")
