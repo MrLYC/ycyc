ab5aa6e2a3575f9077e4a1b7c30ab10b88b3fc5d
MrLYC <imyikong@gmail.com>
2015-07-02 20:28:26 +0800

    add tail_call_optimized

---
+++ tests/contribs/test_tail_call_optimized.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from unittest import TestCase
+
+from ycyc.contribs.tail_call_optimized import tail_call_optimized
+
+
+class TestTailCallOptimized(TestCase):
+    def test_usage(self):
+        @tail_call_optimized
+        def factorial(n, acc=1):
+            "calculate a factorial"
+            if n == 0:
+                return acc
+            return factorial(n-1, n*acc)
+
+        factorial(10000)
+
+        @tail_call_optimized
+        def fib(i, current = 0, next = 1):
+            if i == 0:
+                return current
+            else:
+                return fib(i - 1, next, current + next)
+
+        fib(10000)
---
+++ ycyc/contribs/tail_call_optimized.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# source: http://code.activestate.com/recipes/474088-tail-call-optimization-decorator/
+
+import sys
+from functools import wraps
+
+
+class TailRecurseException(Exception):
+    def __init__(self, args, kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+
+def tail_call_optimized(g):
+    """
+    This function decorates a function with tail call
+    optimization. It does this by throwing an exception
+    if it is it's own grandparent, and catching such
+    exceptions to fake the tail call optimization.
+
+    This function fails if the decorated
+    function recurses in a non-tail context.
+    """
+    @wraps(g)
+    def func(*args, **kwargs):
+        f = sys._getframe()
+        if f.f_back and f.f_back.f_back \
+                and f.f_back.f_back.f_code == f.f_code:
+            raise TailRecurseException(args, kwargs)
+        else:
+            while True:
+                try:
+                    return g(*args, **kwargs)
+                except TailRecurseException as e:
+                    args = e.args
+                    kwargs = e.kwargs
+    return func
