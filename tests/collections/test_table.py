02b1c8b6f12df1600f17765891e7dd6a38b5f0da
MrLYC <imyikong@gmail.com>
2015-08-05 23:17:56 +0800

    add unittest for table

--- tests/collections/test_table.py
+++ tests/collections/test_table.py
@@ -3,6 +3,31 @@
 
 from unittest import TestCase
 
+from ycyc.collections import table
 
-class Test1(TestCase):
-    pass
+
+class TestSimpleTable(TestCase):
+    def test_usage(self):
+        tb = table.simple_table(3, 4, lambda x, y: x * 10 + y)
+        self.assertListEqual(tb[0], [0, 1, 2, 3])
+        self.assertListEqual(tb[1], [10, 11, 12, 13])
+        self.assertListEqual(tb[2], [20, 21, 22, 23])
+
+
+class TestTable(TestCase):
+    def test_usage(self):
+        tb = table.Table(3, 4, 0)
+        self.assertEqual(len(tb.table), 3)
+        self.assertEqual(len(tb.table[0]), 4)
+
+        self.assertEqual(tb[1, 2], 0)
+        tb[1, 2] = 1
+        self.assertEqual(tb[1, 2], 1)
+
+
+class TestSparseTable(TestCase):
+    def test_usage(self):
+        tb = table.SparseTable(3, 4, 0)
+        self.assertEqual(tb[1, 2], 0)
+        tb[1, 2] = 1
+        self.assertEqual(tb[1, 2], 1)
