ca78fca64e0037df9f0c0b20406415f4c627e3cb
MrLYC <imyikong@gmail.com>
2015-08-08 01:20:30 +0800

    add Event

---
+++ ycyc/frameworks/events/models.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from ycyc.base.contextutils import catch
+from ycyc.base.typeutils import SimpleExceptions
+
+EventsExceptions = SimpleExceptions()
+ListenerDuplicatedError = EventsExceptions.ListenerDuplicatedError
+ListenerNoExistedError = EventsExceptions.ListenerNoExistedError
+
+
+class Event(object):
+    def __init__(self):
+        super(Event, self).__init__()
+        self.listeners = []
+
+    def notify(self, *args, **kwg):
+        """
+        Notify all the listeners that event is happended.
+        """
+        listeners = tuple(self.listeners)
+        for listener in listeners:
+            with catch():
+                listener(*args, **kwg)
+
+    def register(self, callback):
+        """
+        Register a callback to this event
+        """
+        if callback in self.listeners:
+            raise ListenerDuplicatedError
+        self.listeners.append(callback)
+
+    def unregister(self, callback):
+        """
+        Unregister a callback from this event
+        """
+        with catch(ValueError, ListenerNoExistedError):
+            self.listeners.remove(callback)
