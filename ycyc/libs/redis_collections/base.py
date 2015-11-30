4326875d3baca86459cde8d27ee7f2cb80153d13
MrLYC <imyikong@gmail.com>
2015-11-30 02:50:20 +0000

    add hash_dict

---
+++ ycyc/libs/redis_collections/base.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+from itertools import chain
+
+
+class RedisCollection(object):
+    @classmethod
+    def factory(cls, *base_args, **base_kwargs):
+        def constructor(*args, **kwargs):
+            kwargs.update(base_kwargs)
+            return cls(*chain(base_args, args), **kwargs)
+        return constructor
---
+++ ycyc/libs/redis_collections/hash_dict.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from collections import MutableMapping
+
+from .base import RedisCollection
+
+
+class RedisHashDict(MutableMapping):
+    def __init__(self, connection, name, *args, **kwargs):
+        self.connection = connection
+        self.name = name
+        super(RedisHashDict, self).__init__(*args, **kwargs)
+
+    def __setitem__(self, key, value):
+        self.connection.hset(self.name, key, value)
+
+    def __getitem__(self, key):
+        value = self.connection.hget(self.name, key)
+        if value is None:
+            raise KeyError("%s not found" % key)
+        return value
+
+    def __delitem__(self, key):
+        if self.connection.hdel(self.name, key) == 0:
+            raise KeyError("%s not found" % key)
+
+    def __iter__(self):
+        return iter(self.connection.hkeys(self.name))
+
+    def __len__(self):
+        return self.connection.hlen(self.name)
+
+    def __contains__(self, key):
+        return self.connection.hexists(self.name, key)
