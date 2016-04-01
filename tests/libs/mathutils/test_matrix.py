8e2ad7e75ee8a078fc6289da28a07e796f9e8851
MrLYC <saber_zodiac@sohu.com>
2016-04-01 17:40:29 +0800

    add matrix.py

---
+++ tests/libs/mathutils/test_matrix.py
@@ -0,0 +1,144 @@
+from unittest import TestCase
+
+from ycyc.libs.mathutils.matrix import Matrix
+
+
+class TestMatrix(TestCase):
+    def assertMatrixEqualAs(self, matrix, table):
+        for mr, tr, r in zip(matrix, table, range(matrix.row_n)):
+            for mc, tc, c in zip(mr, tr, range(matrix.col_n)):
+                self.assertEqual(mc, tc)
+            else:
+                self.assertEqual(c, matrix.col_n - 1)
+        else:
+            self.assertEqual(r, matrix.row_n - 1)
+
+    def test_construction(self):
+        m = Matrix(1, 1)
+        self.assertEqual(m[0, 0], m[0][0])
+        self.assertEqual(m[0, 0], 0)
+
+        m = Matrix(3, 2, [[1], [2, 3]])
+        self.assertMatrixEqualAs(m, [
+            [1, 0],
+            [2, 3],
+            [0, 0]
+        ])
+
+        m = Matrix.from_table([
+            [1, 2, 3],
+            [4, 5, 6],
+        ])
+        self.assertIsInstance(m, Matrix)
+        self.assertEqual(m.row_n, 2)
+        self.assertEqual(m.col_n, 3)
+        self.assertMatrixEqualAs(m, [
+            [1, 2, 3],
+            [4, 5, 6],
+        ])
+
+        m = Matrix.from_table([
+            [1, 2],
+            [4, 5, 6],
+            [7],
+        ], init_val=0)
+        self.assertIsInstance(m, Matrix)
+        self.assertEqual(m.row_n, 3)
+        self.assertEqual(m.col_n, 3)
+        self.assertMatrixEqualAs(m, [
+            [1, 2, 0],
+            [4, 5, 6],
+            [7, 0, 0],
+        ])
+
+    def test_equal(self):
+        m1 = Matrix.from_table([
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+            [10, 11, 12],
+        ])
+        m2 = Matrix.from_table([
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+            [10, 11, 12],
+        ])
+        self.assertEqual(m1, m2)
+
+    def test_multiplication(self):
+        m1 = Matrix.from_table([
+            [1, 2, 3],
+            [4, 5, 6],
+        ])
+        m2 = Matrix.from_table([
+            [1, 4],
+            [2, 5],
+            [3, 6],
+        ])
+        self.assertMatrixEqualAs(m1 * m2, [
+            [14, 32],
+            [32, 77],
+        ])
+
+        m1 = Matrix.from_table([
+            [2, 1],
+            [4, 3],
+        ])
+        self.assertMatrixEqualAs(2 * m1, [
+            [4, 2],
+            [8, 6],
+        ])
+
+        m1 = Matrix.from_table([
+            [20, 20, 18],
+            [24, 16, 27],
+            [21, 19, 22],
+        ])
+        m2 = Matrix.from_table([
+            [12, 1.2],
+            [14, 1.3],
+            [16, 1.5],
+        ])
+        self.assertMatrixEqualAs(m1 * m2, [
+            [808, 77],
+            [944, 90.1],
+            [870, 82.9],
+        ])
+
+        m1 = Matrix.from_table([
+            [2, 4],
+            [1, 2],
+        ])
+        m2 = Matrix.from_table([
+            [2, -2],
+            [-1, 1],
+        ])
+        self.assertMatrixEqualAs(m1 * m2, [
+            [0, 0],
+            [0, 0],
+        ])
+        self.assertMatrixEqualAs(m2 * m1, [
+            [2, 4],
+            [-1, -2],
+        ])
+
+    def test_transpose_matrix(self):
+        m = Matrix.from_table([
+            [1, 2],
+            [3, 4],
+        ]).transpose_matrix()
+        self.assertMatrixEqualAs(m, [
+            [1, 3],
+            [2, 4],
+        ])
+
+        m = Matrix.from_table([
+            [1, 2],
+            [3, 4],
+            [5, 6],
+        ]).transpose_matrix()
+        self.assertMatrixEqualAs(m, [
+            [1, 3, 5],
+            [2, 4, 6],
+        ])
---
+++ ycyc/libs/mathutils/matrix.py
@@ -0,0 +1,150 @@
+from collections import deque
+from itertools import repeat
+import os
+
+
+class Matrix(object):
+    @classmethod
+    def from_table(cls, table, init_val=0):
+        data = []
+        row_n = 0
+        col_n = 0
+
+        for rn, r in enumerate(table):
+            row = []
+            for cn, c in enumerate(r):
+                row.append(c)
+
+                if col_n <= cn:
+                    col_n = cn + 1
+            data.append(row)
+            if row_n <= rn:
+                row_n = rn + 1
+
+        return cls(row_n=row_n, col_n=col_n, matrix=data, init_val=init_val)
+
+    def __init__(self, row_n, col_n, matrix=None, init_val=0):
+        self.init_val = init_val
+        self.row_n = int(row_n)
+        self.col_n = int(col_n)
+
+        matrix = matrix or (
+            repeat(init_val, col_n)
+            for r in range(row_n)
+        )
+        self.data = deque((
+            deque((c for c in r), maxlen=col_n)
+            for r in matrix
+        ), maxlen=row_n)
+
+        self.make_sure_matrix_size()
+
+    def make_sure_matrix_size(self):
+        row_n = self.row_n
+        col_n = self.col_n
+        init_val = self.init_val
+        data = self.data
+
+        if row_n <= 0 or col_n <= 0:
+            raise ValueError("illegal size: [%d, %d]" % (self.row_n, self.row_n))
+
+        for r in range(row_n):
+            try:
+                col = data[r]
+                col_len = len(col)
+                if col_len < col_n:
+                    col.extend(repeat(init_val, col_n - col_len))
+            except IndexError:
+                data.append(deque(repeat(init_val, col_n)))
+
+    def __iter__(self):
+        return iter(self.data)
+
+    def __getitem__(self, idx):
+        if isinstance(idx, (tuple, list)):
+            row_n, col_n = idx
+            return self.data[row_n][col_n]
+        return self.data[idx]
+
+    def __setitem__(self, idx, val):
+        if not isinstance(idx, (tuple, list)):
+            raise ValueError("index type error: %s" % idx)
+
+        row_n, col_n = idx
+        if row_n > self.row_n or col_n > self.col_n:
+            raise IndexError("index error: %s" % idx)
+
+        self.data[row_n][col_n] = val
+
+    def __str__(self):
+        output = []
+        max_width = 0
+
+        for r in self:
+            line = []
+            for c in r:
+                element_str = str(c)
+                line.append(element_str)
+
+                element_str_len = len(element_str)
+                if element_str_len > max_width:
+                    max_width = element_str_len
+
+            else:
+                output.append(line)
+
+        template = "%%%ds" % (max_width + 1)
+        return os.linesep.join(
+            ",".join(template % c for c in r)
+            for r in output
+        )
+
+    def __eq__(self, other):
+        for mr, tr, r in zip(self, other, range(self.row_n)):
+            for mc, tc, c in zip(mr, tr, range(self.col_n)):
+                if mc != tc:
+                    return False
+            else:
+                if c != self.col_n - 1:
+                    return False
+        else:
+            if r != self.row_n - 1:
+                return False
+        return True
+
+    def __repr__(self):
+        return str(self)
+
+    def __mul__(self, other):
+        if self.col_n != other.row_n:
+            raise ValueError(
+                "matrix size error:  [%d, %d] and [%d, %d]" % (
+                    self.row_n, self.col_n, other.row_n, other.col_n,
+                )
+            )
+
+        matrix = Matrix(self.row_n, other.col_n)
+        for i in range(self.row_n):
+            for j in range(other.col_n):
+                elememt = 0
+
+                for k in range(self.col_n):
+                    elememt += self[i, k] * other[k, j]
+                else:
+                    matrix[i, j] = elememt
+
+        return matrix
+
+    def __rmul__(self, other):
+        matrix = Matrix(self.row_n, self.col_n, self)
+        for i in range(self.row_n):
+            for j in range(self.col_n):
+                matrix[i, j] *= other
+        return matrix
+
+    def transpose_matrix(self):
+        matrix = Matrix(self.col_n, self.row_n)
+        for i in range(self.row_n):
+            for j in range(self.col_n):
+                matrix[j, i] = self[i, j]
+        return matrix
